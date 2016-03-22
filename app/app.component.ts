569da1d39055ea94f1f3539f2329ec1fb9564322
Brooke Hurford and Jessica Fix <brookebhurford@gmail.com>
2016-03-22 10:10:52 -0700

    Outputs the child and parent connection to the click event

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,15 +1,40 @@
 // Root component. Holds all models (data) for tasks.
 import { Component } from 'angular2/core';
+import { Component, EventEmitter } from 'angular2/core';
+
+// Child component of AppComponent. Passed a set of tasks to display from AppComponent.
+@Component({
+  selector: 'task-list',
+  inputs: ['taskList'], // 'taskList' array is used again below in directive.
+  outputs: ['onTaskSelect'], // output to create custom event-emitter
+  template: `
+  <h3 *ngFor="#currentTask of taskList" (click)="taskClicked(currentTask)">
+  {{ currentTask.description }}
+  </h3>
+  `
+})
+
+// Child component constructor of AppComponent with click event emitter.
+export class TaskListComponent {
+  public taskList: Task[];
+  public onTaskSelect: EventEmitter<Task>;
+  constructor() {
+    this.onTaskSelect = new EventEmitter();
+  }
+  taskClicked(clickedTask: Task): void {
+    console.log(clickedTask 'child');
+    this.onTaskSelect.emit(clickedTask);
+  }
+}
 
 // View and Controller
 @Component({
   selector: 'my-app',
+  directives: [TaskListComponent], // Changed to pull in child component. Tag changed in template as well.
   template: `
   <div class="container">
     <h1>To-Do List</h1>
-    <h3 *ngFor="#task of tasks" (click)="taskWasSelected(task)">
-      {{ task.description }}
-    </h3>
+    <task-list [taskList]="tasks"(onTaskSelect)="taskWasSelected($event)"></task-list>
   </div>
   `
 })
@@ -26,7 +51,7 @@ export class AppComponent {
     ];
   }
   taskWasSelected(clickedTask: Task): void {
-    console.log(clickedTask);
+    console.log(clickedTask 'parent');
   }
 }
 
