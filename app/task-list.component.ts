a98a90121acc90f0f3bd8b76f689a779e2c03357
Brooke Hurford and Jessica Fix <brookebhurford@gmail.com>
2016-03-22 11:39:51 -0700

    Add new-task component to submit and display a new task on the task-list

--- app/task-list.component.ts
+++ app/task-list.component.ts
@@ -2,13 +2,14 @@ import { Component, EventEmitter } from 'angular2/core';
 import { TaskComponent } from './task.component';
 import { Task } from './task.model';
 import { EditTaskDetailsComponent } from './edit-task-details.component';
+import { NewTaskComponent } from './new-task.component';
 
 // Child component of AppComponent. Passed a set of tasks to display from AppComponent.
 @Component({
   selector: 'task-list',
   inputs: ['taskList'], // 'taskList' array is used again below in directive.
   outputs: ['onTaskSelect'], // output to create custom event-emitter
-  directives: [TaskComponent, EditTaskDetailsComponent],
+  directives: [TaskComponent, EditTaskDetailsComponent, NewTaskComponent],
   templateUrl: 'app/task-list.view.html'
 })
 
@@ -24,5 +25,10 @@ export class TaskListComponent {
     console.log(clickedTask 'child');
     this.selectedTask = clickedTask;
     this.onTaskSelect.emit(clickedTask);
+  }
+  createTask(description: string): void {
+    this.taskList.push(
+      new Task(description, this.taskList.length)
+    );
   }
 }
