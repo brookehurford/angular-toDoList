9e7945ae964493dc8b228d4d7db44b7a336fd71b
Brooke Hurford and Jessica Fix <brookebhurford@gmail.com>
2016-03-22 10:30:31 -0700

    Add child component TaskComponent to perform click event emitter

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,16 +1,31 @@
 // Root component. Holds all models (data) for tasks.
-import { Component } from 'angular2/core';
 import { Component, EventEmitter } from 'angular2/core';
+
+// Child compontent of TaskListComponent.
+@Component({
+  selector: 'task-display',
+  inputs: ['task'],
+  template: `
+  <h3>{{ task.description}}</h3>
+  `
+})
+
+export class TaskComponent {
+  public task: Task;
+}
 
 // Child component of AppComponent. Passed a set of tasks to display from AppComponent.
 @Component({
   selector: 'task-list',
   inputs: ['taskList'], // 'taskList' array is used again below in directive.
   outputs: ['onTaskSelect'], // output to create custom event-emitter
+  directives: [TaskComponent],
   template: `
-  <h3 *ngFor="#currentTask of taskList" (click)="taskClicked(currentTask)">
-  {{ currentTask.description }}
-  </h3>
+  <task-display *ngFor="#currentTask of taskList"
+    (click)="taskClicked(currentTask)"
+    [class.selected]="currentTask === selectedTask"
+    [task]="currentTask">
+  </task-display>
   `
 })
 
@@ -18,11 +33,13 @@ import { Component, EventEmitter } from 'angular2/core';
 export class TaskListComponent {
   public taskList: Task[];
   public onTaskSelect: EventEmitter<Task>;
+  public selectedTask: Task;
   constructor() {
     this.onTaskSelect = new EventEmitter();
   }
   taskClicked(clickedTask: Task): void {
     console.log(clickedTask 'child');
+    this.selectedTask = clickedTask;
     this.onTaskSelect.emit(clickedTask);
   }
 }
